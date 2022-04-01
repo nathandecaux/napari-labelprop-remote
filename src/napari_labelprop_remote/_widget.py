4d579009cb9b35718b81988793aa8b6f6f84f3be
nathandecaux <nathan.decaux@hotmail.fr>
2022-04-01 13:50:05 +0200

    Adding Training widget

--- src/napari_labelprop_remote/_widget.py
+++ src/napari_labelprop_remote/_widget.py
@@ -11,6 +11,48 @@ from magicgui import magic_factory,magicgui
 from magicgui.widgets import Select
 import requests
 from enum import Enum,unique
+import requests
+import json
+from enum import Enum
+import nibabel as ni
+import io
+import numpy as np
+from urllib.parse import urljoin
+import time
+host="10.29.225.156"
+port="5000"
+url = f'http://{host}:{port}'
+
+def create_buf_npz(array_dict):
+    buf = io.BytesIO()
+    np.savez_compressed(buf, **array_dict)
+    buf.seek(0)
+    return buf
+
+def get_ckpts():
+    r=urljoin(url,'list_ckpts')
+    response = requests.get(r).text
+    return response.split(',')
+
+def get_file(url):
+    """
+    Download file without saving from url and return the bytes
+    """
+    return io.BytesIO(requests.get(url).content)
+
+def get_session_info(token):
+    r=urljoin(url,'get_session_info')
+    response = requests.get(r,params={'token':token}).text
+    return json.loads(response)
+
+def session_exists(token):
+    r=urljoin(url,'get_session_list')
+    response = requests.get(r).text
+    if token in response:
+        return True
+    else:
+        return False
+
 class ExampleQWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
@@ -58,13 +100,46 @@ def inference(image: "napari.types.ImageData", labels: "napari.types.LabelsData"
 
     This function will be turned into a widget using `autogenerate: true`.
     """
-
+    r=urljoin(url,'inference')
+    params={'z_axis':z_axis,'label':label,'checkpoint':checkpoint}
+    params=json.dumps(params).encode('utf-8')
+    buf=create_buf_npz({'img':image,'mask':labels})
+    response=requests.post(r,files={'arrays':buf,'params':params})
+    token=response.text
+    buf.close()
+    while not session_exists(token):
+        time.sleep(5)
+    r=urljoin(url,'download_inference')
+    r=r+'?token='+token
+    npz_file=np.load(get_file(r),encoding = 'latin1')
+    Y_up,Y_down,Y_fused=npz_file['Y_up'],npz_file['Y_down'],npz_file['Y_fused']
+    return [((Y_up).astype(int), {'name': 'propagated_up'}, 'labels'), ((Y_down).astype(int), {'name': 'propagated_down'}, 'labels'), ((Y_fused).astype(int), {'name': 'propagated_fused'}, 'labels')]
     # shape=torch.load(checkpoint)['hyper_parameters']['shape'][0]
     # if label==0: label='all'
     # Y_up, Y_down, Y_fused = propagate_from_ckpt(
     #     image, labels, checkpoint, z_axis=z_axis,lab=label,shape=shape)
     # return [((Y_up).astype(int), {'name': 'propagated_up'}, 'labels'), ((Y_down).astype(int), {'name': 'propagated_down'}, 'labels'), ((Y_fused).astype(int), {'name': 'propagated_fused'}, 'labels')]
 
+@magic_factory(pretrained_checkpoint={'choices':['']+get_ckpts()})
+def training(image: "napari.types.ImageData", labels: "napari.types.LabelsData", pretrained_checkpoint: "napari.types.Path" = '', shape: int=256, z_axis: int=0, max_epochs: int=10,checkpoint_name='',pretraining=False) -> "napari.types.LayerDataTuple":
+    """Generate thresholded image.
+
+    This function will be turned into a widget using `autogenerate: true`.
+    """
+    r=urljoin(url,'training')
+    params={'pretrained_ckpt':pretrained_checkpoint,'shape':shape,'z_axis':z_axis,'max_epochs':max_epochs,'name':checkpoint_name,'pretraining':pretraining}
+    params=json.dumps(params).encode('utf-8')
+    buf=create_buf_npz({'img':image,'mask':labels})
+    response=requests.post(r,files={'arrays':buf,'params':params})
+    token=response.text
+    buf.close()
+    while not session_exists(token):
+        time.sleep(5)
+    r=urljoin(url,'download_inference')
+    r=r+'?token='+token
+    npz_file=np.load(get_file(r),encoding = 'latin1')
+    Y_up,Y_down,Y_fused=npz_file['Y_up'],npz_file['Y_down'],npz_file['Y_fused']
+    return [((Y_up).astype(int), {'name': 'propagated_up'}, 'labels'), ((Y_down).astype(int), {'name': 'propagated_down'}, 'labels'), ((Y_fused).astype(int), {'name': 'propagated_fused'}, 'labels')]
 
 #send get request to 10.29.225.156:5000/list_ckpts
 #return list of ckpts
--- test_api.py
+++ test_api.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 import requests
 import json
 from enum import Enum
@@ -5,24 +6,78 @@ import nibabel as ni
 import io
 import numpy as np
 import base64
+from urllib.parse import urljoin
+import zipfile
+import time
+# from urllib.request import urlopen,urlretrieve
+import cgi
+host="10.29.225.156"
+port="5000"
+url = f'http://{host}:{port}'
+
+def create_buf_npz(array_dict):
+    buf = io.BytesIO()
+    np.savez_compressed(buf, **array_dict)
+    buf.seek(0)
+    return buf
 
 def get_ckpts():
-    url = 'http://10.29.225.156:5000/list_ckpts'
-    response = requests.get(url).text
+    r=urljoin(url,'list_ckpts')
+    response = requests.get(r).text
     return response.split(',')
 
-buf = io.BytesIO()
+def get_file(url):
+    """
+    Download file without saving from url and return the bytes
+    """
+    return io.BytesIO(requests.get(url).content)
 
-img=ni.load('/mnt/freebox/Segmentations/sub-17/ses-01/anat/sub-17_ses-1_DIXON6ECHOS-e3.nii.gz').get_fdata().astype('float32')
-mask=ni.load('/mnt/freebox/Segmentations/sub-17/ses-01/anat/seg.nii.gz').get_fdata().astype('uint8')
-np.savez_compressed(buf, img=img,mask=mask)
-buf.seek(0)
-params={'z_axis':2,'label':0,'checkpoint':'test.ckpt'}
+def get_session_info(token):
+    r=urljoin(url,'get_session_info')
+    response = requests.get(r,params={'token':token}).text
+    return json.loads(response)
 
-# Closed the buffer
-params=json.dumps(params).encode('utf-8')
-url="http://10.29.225.156:5000/inference"
-#Send post request to http://10.29.225.156:5000/inference
-response=requests.post(url,files={'arrays':buf,'params':params})
-print(response.text)
-buf.close()
+def session_exists(token):
+    r=urljoin(url,'get_session_list')
+    response = requests.get(r).text
+    print(response)
+    if token in response:
+        return True
+    else:
+        return False
+
+
+
+# buf = io.BytesIO()
+
+# img=ni.load('img.nii.gz').get_fdata().astype('float32')
+# mask=ni.load('mask.nii.gz').get_fdata().astype('uint8')
+# np.savez_compressed(buf, img=img,mask=mask)
+# buf.seek(0)
+# params={'z_axis':2,'label':0,'checkpoint':'test.ckpt'}
+
+# # Closed the buffer
+# params=json.dumps(params).encode('utf-8')
+# r=urljoin(url,'inference')
+# #Send post request to http://10.29.225.156:5000/inference
+# response=requests.post(r,files={'arrays':buf,'params':params})
+# token=response.text
+# buf.close()
+token="7938470b-30bb-4401-8235-00cbbd092eba"
+print(token)
+#Send get request to http://10.29.225.156:5000/download_inference
+while not session_exists(token):
+    time.sleep(5)
+print(get_session_info(token))
+
+r=urljoin(url,'download_inference')
+# response=requests.get(r,params={'token':token})
+r=r+'?token='+token
+# response=request.urlretrieve(r,'/tmp/test.npz')
+
+#Recover arrays
+# print(response.headers.get('content-type'))
+# print(requests.get(urljoin(url,'get_session_list')).text)
+npz_file=np.load(get_file(r),encoding = 'latin1')
+print(np.unique(npz_file['Y_fused']))
+
